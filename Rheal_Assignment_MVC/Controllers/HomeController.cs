d71078fe4b9e5bd2f97186d0b6b6efb79d4b8e3f
b0mbrBee <b0mbrBee@DESKTOP-IKAU7F1>
2020-11-30 16:21:47 +0530

    Intital Run logic implemented

--- Rheal_Assignment_MVC/Controllers/HomeController.cs
+++ Rheal_Assignment_MVC/Controllers/HomeController.cs
@@ -6,6 +6,9 @@ using System.Web.Mvc;
 using Rheal_Assignment_MVC.Models;
 using Rheal_Assignment_MVC.BizRepositories;
 using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using System.Threading.Tasks;
 
 namespace Rheal_Assignment_MVC.Controllers
 {
@@ -13,11 +16,39 @@ namespace Rheal_Assignment_MVC.Controllers
     {
         IBizRepository<Course, int> courseRepo;
         RhealAssignmentDBContext context;
+        ApplicationDbContext appdbContext;
+        private ApplicationSignInManager _signInManager;
+        private ApplicationUserManager _userManager;
 
         public HomeController()
         {
             courseRepo = new CourseBizRepository();
             context = new RhealAssignmentDBContext();
+            appdbContext = new ApplicationDbContext();
+        }
+
+        public ApplicationSignInManager SignInManager
+        {
+            get
+            {
+                return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
+            }
+            private set
+            {
+                _signInManager = value;
+            }
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
         }
 
         public ActionResult SearchResult(string query)
@@ -36,8 +67,47 @@ namespace Rheal_Assignment_MVC.Controllers
 
             return View(result);
         }
-        public ActionResult Index()
+
+        public async Task<ActionResult> Index()
         {
+            //Check for initial RUN
+            if (appdbContext.Users.Count() == 0)
+            {
+                //make new roles for Administrator, Trainer, Student
+                IdentityRole adminrole = new IdentityRole() { Name = "Administrator" };
+                IdentityRole trainerrole = new IdentityRole() { Name = "Trainer" };
+                IdentityRole studentrole = new IdentityRole() { Name = "Student" };
+
+                appdbContext.Roles.Add(adminrole);
+                appdbContext.Roles.Add(trainerrole);
+                appdbContext.Roles.Add(studentrole);
+                appdbContext.SaveChanges();
+
+                //make Default Area of interest entry as "Computer Science"
+                AreaOfInterest aoi = new AreaOfInterest() { Name = "Computer Science" };
+                context.AreaOfInterests.Add(aoi);
+                context.SaveChanges();
+
+                //register new user Administrator
+                RegisterViewModel model = new RegisterViewModel();
+                model.Email = "Administrator@app.com";
+                model.Password = "Password@123456";
+                model.ConfirmPassword = "Password@123456";
+                string Role = "Administrator";
+
+                ApplicationUser user = new ApplicationUser();
+                user.UserName = "Administrator@app.com";
+                user.Email = "Administrator@app.com";
+
+                var res = await UserManager.CreateAsync(user, model.Password);
+                //assign administrator user to Administrator role
+                UserManager.AddToRole(user.Id, Role);
+
+
+            }
+
+
+
             //get all courses and show courses
             var result = courseRepo.getData();
 

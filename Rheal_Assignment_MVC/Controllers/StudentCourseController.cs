a2ec462d3d6ca2f44a18242037ecb75b817f70be
b0mbrBee <b0mbrBee@DESKTOP-IKAU7F1>
2020-11-30 16:40:04 +0530

    Make sure Student can only enroll in the same course once.

--- Rheal_Assignment_MVC/Controllers/StudentCourseController.cs
+++ Rheal_Assignment_MVC/Controllers/StudentCourseController.cs
@@ -19,19 +19,34 @@ namespace Rheal_Assignment_MVC.Controllers
             context = new RhealAssignmentDBContext();
         }
 
-        //authorize so only registered students and enroll
+        public ActionResult showCourse(int course_id)
+        {
+            //show course Details
+            return View();
+        }
+
+        //authorize so only registered students can enroll
         [Authorize(Roles = "Student")]
         public ActionResult EnrollIntoCourse(int course_id, int student_id)
         {
+
             //prepare object for making an entry in database
             StudentCourse cs = new StudentCourse();
             cs.CourseId = context.Courses.Where(e => e.CourseRowId == course_id).FirstOrDefault();
             cs.StudentId = context.Students.Where(e => e.StudentRowId == student_id).FirstOrDefault();
             cs.isCompleted = false;
-            //create row in database
-            //studentCourseRepo.create(cs);
-            context.StudentCourses.Add(cs);
-            context.SaveChanges();
+
+            //check if student has already enrolled in the same course
+            List<StudentCourse> temp = context.StudentCourses.Where(e => e.StudentId.StudentRowId == student_id).ToList();
+            StudentCourse temp2 = temp.Where(e => e.CourseId.CourseRowId == course_id).FirstOrDefault();
+            if (temp2 == null)
+            {
+                //create row in database
+                //studentCourseRepo.create(cs);
+                context.StudentCourses.Add(cs);
+                context.SaveChanges();
+            }
+            
 
             return RedirectToAction("Index", "Home");
         }
